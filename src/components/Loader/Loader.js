24b1105d0f720213466b0a4b417197e7c5339e65
Angel <abadrimsim@gmail.com>
2022-03-11 00:03:44 +0800

    Add preloader and custom scrollbar

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,11 @@
+import styles from './Loader.module.scss';
+
+function Loader() {
+	return (
+		<div className={styles.loader}>
+			<h1>Felicità Ristorante</h1>
+		</div>
+	);
+}
+
+export default Loader;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,27 @@
 import '../styles/globals.scss';
 import SimpleReactLightbox from 'simple-react-lightbox';
+import { useState, useEffect } from 'react';
+import Loader from '../components/Loader/Loader';
 
 function MyApp({ Component, pageProps }) {
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		setLoading(true);
+		window.addEventListener('load', () => {
+			setLoading(false);
+		});
+	}, []);
+
 	return (
 		<>
-			<SimpleReactLightbox>
-				<Component {...pageProps} />
-			</SimpleReactLightbox>
+			{!loading ? (
+				<SimpleReactLightbox>
+					<Component {...pageProps} />
+				</SimpleReactLightbox>
+			) : (
+				<Loader />
+			)}
 		</>
 	);
 }
